13a8ed1a25699d75b074a4e7aca1c3ea0e747e81
quyanna <quyannacampbell@gmail.com>
2022-02-19 16:31:02 -0700

    added table to the display

--- CHUT/transitData/views.py
+++ CHUT/transitData/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("""<table><tr><th>Station</th>
+        <th>Line</th>
+        <th>Time</th>
+    </tr>
+    <tr>
+        <td>example station</td>
+        <td>example line</td>
+        <td>example time</td>
+    </tr>
+</table>""")
 
