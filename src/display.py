6938ad5ffb92f2a114379ac073407d3e157924b4
meebee70 <christopher.kozbial@ucalgary.ca>
2022-02-19 18:23:28 -0700

    time junk

--- src/display.py
+++ src/display.py
@@ -1,14 +1,42 @@
 from data import ArrivalData
+from datetime import datetime
+
+LENGTH = 50
 
 def display(data):
 
-    print("Station" + (" " * 10) + "Line" + (" "*13) + "Time")
+    print("Station" + (" " * (LENGTH - 7)) + "Line" + (" "*(LENGTH - 4)) + "Time")
+
+    now = datetime.now()
 
     for i in data:
-        print(i.station + (" " * (17-len(i.station))) + i.line + (" " * (17-len(i.line))) + str(i.time))
+
+        timem = 0
+        times = 0
+        timeh = i.hour - now.hour
+        if (i.minute - now.minute < 0):
+            timeh -= 1
+            timem = now.minute - i.minute
+        else:
+            timem = i.minute - now.minute
+
+        timestr = str(i.hour - now.hour) + ":"
+
+        if (i.second - now.second):
+            timem -= 1
+            times = now.second - i.second
+        else:
+            times = i.second - now.second
+
+        timestr += str(timem) + ";" + str(times)
 
 
-a = ArrivalData({"station":"bob","line":"greg","hour":20,"minute":30,"second":20})
-b = ArrivalData({"station":"bob2","line":"greg","hour":20,"minute":30,"second":20})
+        timestr += str(i.minute - now.minute) + ";"
+        timestr += str(i.second - now.second) + ";"
+        print(i.station + (" " * (LENGTH-len(i.station))) + i.line + (" " * (LENGTH-len(i.line))) + timestr)
+
+
+a = ArrivalData({"station":"Univeristy Station","line":"69th Street","hour":20,"minute":30,"second":20})
+b = ArrivalData({"station":"University Station","line":"Tuscany","hour":20,"minute":30,"second":20})
 
 display([a,b])
