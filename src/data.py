ff61385c97fcff6fd779aa35b6b527be3e003ad2
Mark Holum <mark.holum@ucalgary.ca>
2022-02-19 20:13:17 -0700

    fucking c++

--- src/data.py
+++ src/data.py
@@ -47,7 +47,7 @@ def get_next_arrival_times(station, current_time, number_of_arrivals):
 	num_rcved = 0
 	while num_rcved < reply_size:
 		json_datab = connection.recv(reply_size)
-		num_rcved += json_data.size()
+		num_rcved += len(json_data)
 		if not json_data:
 			break
 	json_data = json_datab.decode("ascii")
